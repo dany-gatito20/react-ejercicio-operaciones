f904b6a39ce376c3d489b5836dffa6db94fab3fa
Daniel Alvarez <danielalejandroalvarezmorales@gmail.com>
2021-03-01 11:38:39 -0600

    docs agregado

---
+++ docs/precache-manifest.ed5739ea372ce6f11ce171a1a6daf998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad6d150fff2f6722cd6d81c57840ae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25d401eff9d21ef9d6b7",
+    "url": "/static/css/main.fb6250c3.chunk.css"
+  },
+  {
+    "revision": "0ac8c24ca3014fc72311",
+    "url": "/static/js/2.9ecefdf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ecefdf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d401eff9d21ef9d6b7",
+    "url": "/static/js/main.0267d2b3.chunk.js"
+  },
+  {
+    "revision": "71e9ffc85ea187a73f31",
+    "url": "/static/js/runtime-main.3af0f3be.js"
+  }
+]);
